0abbe9588256f13ddcdc73234d7faab3629e1f5a
Sanoj Kumar <sanojkumar327@gmail.com>
2024-08-25 18:44:50 +0530

    Personal Link-Tree

---
+++ script.js
@@ -0,0 +1,21 @@
+// Add event listeners to the buttons
+document.getElementById("cv").addEventListener("click", function() {
+    window.location.href = "https://drive.google.com/file/d/1pBtL-QfHk8cQMq3rqoGJNl5KvRGY3Q1U/view?usp=drive_link"; // Replace with the actual URL for CV
+});
+
+document.getElementById("email").addEventListener("click", function() {
+    window.location.href = "mailto:sanojkr0864@gmail.com"; // Replace with the actual email address
+});
+
+document.getElementById("projects").addEventListener("click", function() {
+    window.location.href = "https://github.com/sanoj-kr?tab=repositories"; // Replace with the actual URL for Power BI Projects
+});
+
+document.getElementById("github").addEventListener("click", function() {
+    window.location.href = "https://github.com/sanoj-kr"; // Replace with the actual GitHub profile link
+});
+
+document.getElementById("instagram").addEventListener("click", function() {
+    window.location.href = "https://instagram.com/raazan0864"; // Replace with the actual Instagram profile link
+});
+
